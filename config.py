a26a5d6cdec324a0dd1cf767c393443fe4fca6ab
airbirdx <mqliutong@163.com>
2019-03-21 20:51:10 +0800

    test blank folder

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # CONFIG PARAMETER
-run_times = 8
+run_times = 2
 # run_materials = 5
 # run_apples = ['Ag', 1]
 
-# default_support = 0
-# default_craft_manpo = 2
+default_support = 1
+default_craft_manpo = 2
 
 # default_servant_priority = [0, 1, 3, 2] 
 default_color_priority = 'RBG'
-default_skill = '(1)q(2)o'
-default_final = '(1)cxx(2)bxx(3)axx'
+default_skill = '(1)p(3)abc32iko32q'
+default_final = '(3)bxx'
 default_final_unit = 'round'
 # speed_ratio = 1   # normal(=1), faster(>1), slower(<1)
 default_chain = 1
